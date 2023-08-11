791e94b3523c73a5b1410b4b05440152e2734f3d
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-08-11 16:46:03 +0530

    Add files via upload

---
+++ src/js/cjs/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var Alert = require("./alert");
+var WarningAlert = require("./warning-alert");
+var ErrorAlert = require("./error-alert");
+var Prompt = require("./prompt");
+
+exports.Alert= Alert;
+exports.WarningAlert= WarningAlert;
+exports.ErrorAlert= ErrorAlert;
+exports.Prompt= Prompt;
