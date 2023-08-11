69b692a2104d5f4d6591becdc3b9b14afb5f4e72
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-08-11 16:45:20 +0530

    Add files via upload

---
+++ src/js/esm/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import Alert from "./alert";
+import WarningAlert from "./warning-alert";
+import ErrorAlert from "./error-alert";
+import Prompt from "./prompt";
+
+export {
+  Alert,
+  WarningAlert,
+  ErrorAlert,
+  Prompt
+}
